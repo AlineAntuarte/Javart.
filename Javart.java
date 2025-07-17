24f87e88e595b4b411c823030610e7fe1de73ad1
Antuarte <aline.ba@aluno.ifsc.edu.br>
2025-07-17 15:26:44 -0300

    Olá, Mundo!

--- Javart.java
+++ Javart.java
@@ -1,12 +1,11 @@
-// Primeiro código em Java
-// Autor: [Aline Antuarte]
-// Data: 30-06-2025 
-
-public class Javart {
-    public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
-        System.out.println("Meu nome é Aline Antuarte.");
-        System.out.println("Estou aprendendo Java.");
-        System.out.println("Este é o meu primeiro código em Java.");
+package primeiroprograma; // É opcional quando for fazer o primeiro programa.
+// Isso "{} é uma chave, é importante ajustar as abertas '{' com uma '}' ao final para fechar".
+// As chaves na maioria das linguagens são blocos, tudo que está em seu interior é considerado um bloco/tijolo.
+public class PrimeiroPrograma{
+    //Dentro do bloco da classe (class) vamos criar um método, que é o método principal (main).
+    public static void main (String [] args){
+        // Bom, até aqui temos um método PRINCIPAL/MAIN dentro da CLASSE/CLASS "PrimeiroPrograma",
+        //dentro do PACOTE/PACKAGE "primeiroprograma".
+        System.out.print("Olá, Mundo!")
     }
 }
