9336b7356b8554996e17c07bbe106c3ae2dbdcd2
Antuarte <aline.ba@aluno.ifsc.edu.br>
2025-07-25 15:20:09 -0300

    Atualização e renomeação de Javart.java para PrimeiroPrograma.java

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package primeiroprograma; // Todas as palavras minúsculas? É um pacote!
+public class PrimeiroPrograma{ //Primeira letra maiúscula? É uma classe ou interface!
+    public static void main (String [] args){ //main é uma palavra que começa com minúscula, é um método.
+        System.out.print("Olá, Mundo!"); //print não pula linha.
+        System.out.println("Olá, Mundo!"); //println pula uma linha.
+        // System começa com letra maiúscula, é uma classe.
+        // O System.out.print não significa "escreva na tela do computador", significa "escreva em qualquer coisa"
+        // System é Sistema, e para o Java, pode ser qualquer sistema. out é saída, print é imprimir.
+    }
+}
+
+// Se a letra inicial da 1° palavra for minúscula e do resto CamelCase, é um atributo, variável ou método, e não uma classe.
+// Atributo - nomeAluno
+// Variável - mediaPrimeiroBimestre
+// Método - lancarNotaAluno
+// TUDO MAIÚSCULO? É CONSTANTE, ou seja, não muda o valor. Exemplo: VALOR_DE_PI
